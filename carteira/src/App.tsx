4c411bf36cf9adc73cc6066c668ac3c798b96c27
andreCaetano225 <andre.caetano2007@hotmail.com>
2021-03-29 23:13:43 -0300

    dashboard primary

---
+++ carteira/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Glogal from './styles/Global';
+
+import Layout from './components/Layout'
+const App: React.FC = () => {
+    return(
+        <div>
+        <Glogal/>
+        <Layout/>
+        </div>
+    );
+}
+
+export default App;
---
+++ carteira/src/styles/themes/light.ts
@@ -0,0 +1,16 @@
+export default {
+    title: 'Light' ,
+    color:{
+        primary: '#DCDCDC',
+        secondary: '#FFFFFF',
+        tertiary: '#F5F5F5',
+
+        white: '#000',
+        black: '#FFF',
+        gray: '#BFBFBF',
+
+        success:'#03BB85',
+        infor:'#3B5998',
+        warning: '#FF6961',
+    }
+}
